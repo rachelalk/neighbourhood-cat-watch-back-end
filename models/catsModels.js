cdcd3581ba1cf963729db6ed472625f50700cd8a
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-10-18 10:47:18 +0100

    set up post and patch models and routes

--- models/catsModels.js
+++ models/catsModels.js
@@ -1,8 +1,46 @@
 import { query } from "../db/index.js";
 
 export async function getAllCats() {
-	const result = await query (`SELECT * FROM cats;`);
-	return result.rows
+	const result = await query(`SELECT * FROM cats;`);
+	return result.rows;
 }
 
+export async function addNewCat(newCat) {
+	const {
+		cat_id,
+		lat,
+		long,
+		friendliness,
+		cuteness,
+		comment,
+		photo_url,
+		still_there,
+		not_there,
+	} = newCat;
 
+	const data = await query(
+		`INSERT INTO cats (cat_id, lat, long, friendliness, cuteness, comment, photo_url, still_there, not_there) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);`,
+		[
+			cat_id,
+			lat,
+			long,
+			friendliness,
+			cuteness,
+			comment,
+			photo_url,
+			still_there,
+			not_there,
+		]
+	);
+	return data.rows;
+}
+
+export async function stillThere(id, stillThere, notThere) {
+	const { cat_id } = id;
+	const { still_there } = stillThere;
+	const { not_there } = notThere;
+	const data = await query(
+		`UPDATE cats SET still_there = $1 WHERE, not_there = $2 cat_id = $3 RETURNING *;`,
+		[Number(still_there), Number(not_there), Number(cat_id)]
+	);
+}
--- routes/catsRouter.js
+++ routes/catsRouter.js
@@ -1,9 +1,8 @@
-import express, {Router} from "express";
+import express, { Router } from "express";
 import cors from "cors";
-import {query} from "../db/index.js"
-import {getAllCats} from "../models/catsModels.js"
+import { query } from "../db/index.js";
+import { getAllCats, addNewCat, stillThere } from "../models/catsModels.js";
 const catsRouter = express.Router(cors());
-
 
 catsRouter.get("/", async function (req, res) {
 	const result = await getAllCats();
@@ -12,6 +11,23 @@ catsRouter.get("/", async function (req, res) {
 		message: "displaying all cats",
 		payload: result,
 	});
+});
+
+catsRouter.post("/", async function (req, res) {
+	const newCat = req.body;
+	const results = await addNewCat(newCat);
+	res.json({
+		success: true,
+		message: `new cat added`,
+		payload: results,
+	});
+});
+
+catsRouter.patch("/id", async function (req, res) {
+	const id = Number(req.params.id);
+	const data = req.body;
+	const result = await stillThere(id, data);
+	res.json({ success: true, payload: result });
 });
 
 export default catsRouter;
